7861e5d8e190d67221c69c029cc9ef45e523c8df
sheiswiz <dapneruth304@gmail>
2022-04-01 22:45:47 +0300

    adding values to the export module

--- shoppingCart.js
+++ shoppingCart.js
@@ -2,3 +2,9 @@ console.log('exporting  module');
 
 const shippingCost = 10;
 const cart = [];
+export const addToCart = function (product, quantity)
+{
+
+    cart.push({product,quantity});
+    console.log(`${quantity} ${product} added to cart`);
+}
