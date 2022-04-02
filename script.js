6316aacb8d349526b587ed6d60a98bbd0e8273a3
sheiswiz <dapneruth304@gmail>
2022-04-02 05:25:39 +0300

    fixed some bugs

--- script.js
+++ script.js
@@ -25,4 +25,4 @@ console.log(cart)
  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
  const data = await res.json();
 console.log(data);
- console.log('Someth
+ console.log('Something');
