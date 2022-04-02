29f28904d0adf11093bf9d756f8ea77c7c1cd5fa
sheiswiz <dapneruth304@gmail>
2022-04-02 05:50:17 +0300

    module patern

--- script.js
+++ script.js
@@ -28,7 +28,8 @@ console.log(data);
  console.log('Something');
  */
 
- const getLastPost = async function () {
+ /*
+const getLastPost = async function () {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
     const data = await res.json();
   
@@ -37,4 +38,19 @@ console.log(data);
   
   const lastPost = getLastPost();
   console.log(lastPost);
+  */
+
+  /////////////MODULE PATTERN////////
+
+  const ShoppingCart2 = (function () {
+    const cart = [];
+    const shippingCost = 10;
+    const totalPrice = 237;
+    const totalQuantity = 23;
   
+    const addToCart = function (product, quantity) {
+      cart.push({ product, quantity });
+      console.log(
+        `${quantity} ${product} added to cart`);
+      };
+    });
