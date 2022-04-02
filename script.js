9155bbc4780d397462d7e612426473ce64c0e3bf
sheiswiz <dapneruth304@gmail>
2022-04-02 05:09:55 +0300

    defaultfunction

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 
 
-import { addToCart, totalPrice as price, totalquantity } from './shoppingCart.js';
+//import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 
-console.log('Importing module')
+//console.log('Importing module')
 
- addToCart('bread', 5);
- console.log(price, totalquantity);
+ //addToCart('bread', 5);
+ //console.log(price, tq);
+
+ //import * as ShoppingCart from './shoppingCart.js';
+ //ShoppingCart.addToCart('bread', 5);
+
+ //console.log(ShoppingCart.totalPrice);
+ 
+ import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
+
+console.log(cart)
