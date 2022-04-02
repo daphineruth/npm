300dac9a0295663eae7fb24cd0e21fdd28a96886
sheiswiz <dapneruth304@gmail>
2022-04-02 18:58:08 +0300

    bundling with parcel and Npm script

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,15 +1,25 @@
-console.log('exporting  module');
+// Exporting module
+console.log('Exporting module');
+
+// Blocking code
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching users');
 
 const shippingCost = 10;
-const cart = [];
-export const addToCart = function (product, quantity)
-{
+export const cart = [];
 
-    cart.push({product,quantity});
-    console.log(`${quantity} ${product} added to cart`);
-}
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
 const totalPrice = 237;
 const totalQuantity = 23;
 
 export { totalPrice, totalQuantity as tq };
 
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
