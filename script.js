bfb9dcacb74e40fda3c4105759fa2436c8836d6c
sheiswiz <dapneruth304@gmail>
2022-04-02 14:32:14 +0300

    altered way of accessing npm package

--- script.js
+++ script.js
@@ -73,14 +73,14 @@ console.log(ShoppingCart2);
 console.log(ShoppingCart2.shippingCost);
 */
 
-import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
 const state = {
   cart: [
     { product: 'bread', quantity: 5 },
     { product: 'pizza', quantity: 5 },
   ],
-  user: { loggedIn: true },
-};
+  user: { loggedIn: true},
+}
 const stateClone = Object.assign({}, state);
 const stateDeepClone = cloneDeep(state);
 
@@ -88,3 +88,5 @@ state.user.loggedIn = false;
 console.log(stateClone);
 
 console.log(stateDeepClone);
+
+
